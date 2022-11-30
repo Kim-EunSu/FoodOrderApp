3ee0db933b1235dfa91a6462c213350b99f5436b
Kim-EunSu <kes73640@gmail.com>
2022-11-30 18:38:44 +0900

    useRef 사용

--- src/components/Meals/MealsItem/MealItemForm.js
+++ src/components/Meals/MealsItem/MealItemForm.js
@@ -1,10 +1,35 @@
+import { useRef, useState } from "react";
 import Input from "../../UI/input";
 import classes from "./MealItemForm.module.css";
 
 const MealsItemForm = (props) => {
+  // 에러메세지
+  const [amountIsValid, setAmountIsValid] = useState(true);
+
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      return setAmountIsValid(false);
+    }
+
+    // prop을 가져올 함수 호출
+    props.onAddToCart(enteredAmountNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount",
@@ -16,6 +41,7 @@ const MealsItemForm = (props) => {
         }}
       />
       <button>+ ADD</button>
+      {!amountIsValid && <p>Please enter a valid amount(1-5)</p>}
     </form>
   );
 };
