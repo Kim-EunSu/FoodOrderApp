a4844c429df79dcc8a00c2092cda0ea7812f90ad
Kim-EunSu <kes73640@gmail.com>
2022-12-08 12:22:21 +0900

    1208

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,16 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import CartContext from "../../store/cart-context";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 import CartItem from "./CartItem";
+import Checkout from "./Checkout";
 
 const Cart = (props) => {
+  const [isCheckout, setIsCheckout] = useState(false);
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [didSubmit, setDidSubmit] = useState(false);
+
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -17,6 +23,27 @@ const Cart = (props) => {
 
   const cartItemRemoveHandler = (id) => {
     cartCtx.removeItem(id);
+  };
+
+  const orderHandler = () => {
+    setIsCheckout(true);
+  };
+
+  const submitOrderHandler = async (userData) => {
+    setIsSubmitting(true);
+    await fetch(
+      "https://foodorderapp-cb49d-default-rtdb.firebaseio.com/order.json",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          user: userData,
+          orderItmes: cartCtx.items,
+        }),
+      }
+    );
+    setIsSubmitting(false);
+    setDidSubmit(true);
+    cartCtx.clearCart();
   };
 
   const cartItems = (
@@ -34,19 +61,51 @@ const Cart = (props) => {
     </ul>
   );
 
-  return (
-    <Modal onClose={props.onClose}>
+  const modalActions = (
+    <div className={classes.actions}>
+      <button className={classes["button--alt"]} onClick={props.onClose}>
+        Close
+      </button>
+      {hasItems && (
+        <button className={classes.button} onClick={orderHandler}>
+          Order
+        </button>
+      )}
+    </div>
+  );
+
+  const cartModalContent = (
+    <>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
+      {isCheckout && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
+      )}
+      {!isCheckout && modalActions}
+    </>
+  );
+
+  const isSubmittingModalContent = <p> Sending order data</p>;
+
+  const didSubmitModalContent = (
+    <>
+      <p>Successfully sent the order!</p>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]} onClick={props.onClose}>
+        <button className={classes.button} onClick={props.onClose}>
           Close
         </button>
-        {hasItems && <button className={classes.button}>Order</button>}
       </div>
+    </>
+  );
+
+  return (
+    <Modal onClose={props.onClose}>
+      {!isSubmitting && !didSubmit && cartModalContent}
+      {isSubmitting && isSubmittingModalContent}
+      {!isSubmitting && didSubmit && didSubmitModalContent}
     </Modal>
   );
 };
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -15,38 +15,41 @@ const AvailableMeals = () => {
       );
 
       if (!response.ok) {
-        throw new Error("Something went wrong");
+        throw new Error("something went wrong!");
       }
 
       const responseData = await response.json();
 
-      const loadedMeals = [];
+      const loadMeals = [];
 
       for (const key in responseData) {
-        loadedMeals.push({
+        loadMeals.push({
           id: key,
           name: responseData[key].name,
           description: responseData[key].description,
           price: responseData[key].price,
         });
       }
-      setMeals(loadedMeals);
+      setMeals(loadMeals);
       setIsLoading(false);
     };
-
-    // try {
-    //    fetchMeals();
-    // } catch (error) {
-    //   setIsLoading(false);
-    //   setHttpError(error.message);
-    // }
-
-    //promise내부의 오류를 다룰 수 있는 promise만이 가능한 기존방법
     fetchMeals().catch((error) => {
       setIsLoading(false);
       setHttpError(error.message);
     });
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return <section className={classes.MealsError}>{httpError}</section>;
+  }
 
   const mealList = meals.map((meal) => (
     <MealItem
@@ -57,22 +60,6 @@ const AvailableMeals = () => {
       price={meal.price}
     />
   ));
-
-  if (isLoading) {
-    return (
-      <section className={classes.MealsLoading}>
-        <p>...Loading</p>
-      </section>
-    );
-  }
-
-  if (httpError) {
-    return (
-      <section className={classes.MealsError}>
-        <p>{httpError}</p>
-      </section>
-    );
-  }
 
   return (
     <section className={classes.meals}>
